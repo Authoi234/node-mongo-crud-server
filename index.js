e3534878812a83a1ca4d28e37447dd9478809677
Authoi234 <jawadkhan.authoi@gmail.com>
2023-11-10 01:13:16 +0630

    post and some mongo db system is ready

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ const port = process.env.PORT || 5000;
 
 // middleware
 app.use(cors());
+app.use(express.json())
 
 // user name : dbUser2
 // password : 5PSu0HxJT38636NX
@@ -21,26 +22,26 @@ const client = new MongoClient(uri, {
 });
 
 async function run() {
-    try{
-        const userCollection = client.db('nodeMongoCrud').collection('users');
-        const user = {
-          name: 'testing test',
-          email: 'testing@gmail.com'
-        }
-        const result = await userCollection.insertOne(user);
-        console.log(result);  
-    }
-    finally{
+  try {
+    const userCollection = client.db('nodeMongoCrud').collection('users');
 
-    }
+    app.post('/users', async (req, res) => {
+      const user = req.body;
+      const result = await userCollection.insertOne(user)
+      res.send(result);
+    })
+  }
+  finally {
+
+  }
 }
 run().catch(err => console.log(err));
 
 
 app.get('/', (req, res) => {
-    res.send('Hello from node mongo crud server');
+  res.send('Hello from node mongo crud server');
 });
 
 app.listen(port, () => {
-    console.log('Listening to port', port);
+  console.log('Listening to port', port);
 })
